35b1e1edd6e8c764fad45d497bbcb6ae9bc1f9c0
Jason Rubenstein <jasonr34@outlook.com>
2020-12-19 02:07:44 -0500

    Update day1.py

--- day1.py
+++ day1.py
@@ -1,12 +1,28 @@
-
-def day1(numbers, target):
+def day1A(numbers, target):
     for i, number in enumerate(numbers[:-1]):
         complement = target - number
         if complement in numbers[i+1:]:
             print("Solution: {} and {}; product = {}".format(number, complement, number*complement))
             break
-    else:  # note 3
+    else:
         raise ValueError("No solutions exist")
-        
+
+
+def day1B(number_list, numbers_len, target):
+    for i in range(0, numbers_len - 2):
+
+        for j in range(i + 1, numbers_len - 1):
+
+            for k in range(j + 1, numbers_len):
+                if number_list[i] + number_list[j] + number_list[k] == target:
+                    print("Solution: ", number_list[i],
+                          ",", number_list[j], ",", number_list[k])
+                    print("Product = ", number_list[i] * number_list[j] * number_list[k])
+                    return True
+
+    return False
+
+
 if __name__ == "__main__":
-    day1([...],2020)
+    day1A(numbers,2020)
+    day1B(numbers, len(numbers), 2020)
