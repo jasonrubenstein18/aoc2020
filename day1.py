5638b646c4627e05d7f937bfbc6d71d90aaf75d6
Jason Rubenstein <jasonr34@outlook.com>
2020-12-19 01:52:12 -0500

    Update day1.py

--- day1.py
+++ day1.py
@@ -1,7 +1,12 @@
-for i, number in enumerate(numbers[:-1]):
-    complement = target - number
-    if complement in numbers[i+1:]:
-        print("Solution: {} and {}; product = {}".format(number, complement, number*complement))
-        break
-else:  # note 3
-    raise ValueError("No solutions exist")
+
+def day1(numbers, target):
+    for i, number in enumerate(numbers[:-1]):
+        complement = target - number
+        if complement in numbers[i+1:]:
+            print("Solution: {} and {}; product = {}".format(number, complement, number*complement))
+            break
+    else:  # note 3
+        raise ValueError("No solutions exist")
+        
+if __name__ == "__main__":
+    subset_sum([...],2020)
